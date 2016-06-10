be84531ae8a1093576d7c8d46586bff8d5e5a510
Alex Saveliev <lyolik@codeminders.com>
2016-06-10 22:50:21 +0300

    Test case for sourcegraph/srclib-python#56

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='python-unresolved-reqstxt',
+    version='',
+    url='http://github.com/sgtest/python-unresolved-reqstxt',
+    py_modules=['mod123'],
+)
